b8ce7a822399d6d222e4c625f0e6a32799dc4a56
Anthony Saraïs <anthony.sarais@greencom-networks.com>
2017-10-20 12:11:34 +0200

    updated code to make it work with react-create-class

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Example from './example.js';
+'use strict';
 
-ReactDOM.render(<Example/>, document.getElementById('demo'));
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _example = require('./example.js');
+
+var _example2 = _interopRequireDefault(_example);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_reactDom2.default.render(_react2.default.createElement(_example2.default, null), document.getElementById('demo'));
